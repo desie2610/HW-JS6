25b6dab29e11fb395bdfcf0b765f8c3f6b0b9063
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-29 21:33:26 +0300

    fgfgfgfgfg

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i ++;
+// }
+
+// for (let i = 2; i <= 20; i ++) {
+//     if (i % 2 === 0) continue; 
+//     console.log(i);           
+//   }
+
+
+// for (let i = 1; i <= 10; i += 1) {
+//     console.log(`7 x ${i} = ${7 * i}`);
+// }
+
+// let n = Number(prompt("Введіть число n:"));
+
+// for (let i = 100; i < n; i++) {
+//    if (n < i) {
+//    break;
+//    }
+
+//   console.log(i);
+// } 
+
+
+// let i = 1;
+
+// while (i <= 20) {
+//   if (i % 3 === 0) {
+//     i++;
+//     continue; 
+//   }
+//   console.log(i);
+//   i++;
+// }
